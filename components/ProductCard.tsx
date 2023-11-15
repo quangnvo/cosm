1e0e60956361aae8f1f2195e9655197d0e37e772
quangnvo <voquang.usth@gmail.com>
2023-11-15 22:53:21 +0800

    complete the draft of the ProductCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -6,22 +6,41 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import Image from 'next/image';
+import { Badge } from "@/components/ui/badge"
 
 
 const ProductCard = () => {
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
-        </Card>
+        <div>
+            <div className="flex justify-center">
+                {/* <div className="w-10 h-10 bg-slate-900 rounded-full flex items-center justify-center mb-2">
+                <span className="text-white font-semibold">1</span>
+            </div> */}
+                <div className="px-3 py-2 inline-block rounded-lg mb-2 bg-pink-200 shadow-sm">
+                    <span>Step 1</span>
+                </div>
+            </div>
+
+            <Card>
+                <CardHeader>
+                    <Image
+                        src="https://picsum.photos/200"
+                        alt="Product Image"
+                        width={200}
+                        height={150}
+                    />
+                </CardHeader>
+
+                <CardContent>
+                    <p>The Ordinary Hyaluronic Acid 2% + B5</p>
+                </CardContent>
+
+                <CardFooter>
+                    <Badge>Serum</Badge>
+                </CardFooter>
+            </Card>
+        </div>
     )
 }
 
--- components/RoutineCard.tsx
+++ components/RoutineCard.tsx
@@ -2,7 +2,10 @@ import ProductCard from "@/components/ProductCard";
 
 const RoutineCard = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-7">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
             <ProductCard />
             <ProductCard />
             <ProductCard />
