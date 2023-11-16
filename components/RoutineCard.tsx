eb79394bbdecaa2d1a7f18071ebb8762cdd15449
quangnvo <voquang.usth@gmail.com>
2023-11-16 09:44:56 +0800

    re-begin the datepicker

--- components/RoutineCard.tsx
+++ components/RoutineCard.tsx
@@ -1,14 +1,13 @@
 import ProductCard from "@/components/ProductCard";
+import DatePicker from "@/components/DatePicker";
 
 const RoutineCard = () => {
     return (
         <div className="p-7 bg-routinecard shadow-md rounded-lg border border-gray-200 flex flex-col gap-6">
-            <div>
-                <p>Date picker here</p>
-                <div className="px-3 py-2 inline-block rounded-lg mb-2 bg-black shadow-sm">
-                    <span className="font-medium text-white">Start</span>
-                </div>
-            </div>
+
+            {/* Date Picker */}
+            <DatePicker />
+
             <div className="grid grid-cols-1 md:grid-cols-4 gap-5 rounded-md">
                 <ProductCard />
                 <ProductCard />
