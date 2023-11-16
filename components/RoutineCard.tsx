9bd4f7c3e1623442951cfdd3a233e1312beb492d
quangnvo <voquang.usth@gmail.com>
2023-11-16 09:52:41 +0800

    add the datepicker

--- components/RoutineCard.tsx
+++ components/RoutineCard.tsx
@@ -1,12 +1,13 @@
 import ProductCard from "@/components/ProductCard";
-import DatePicker from "@/components/DatePicker";
+import DatePickerCard from "@/components/DatePickerCard";
 
 const RoutineCard = () => {
     return (
         <div className="p-7 bg-routinecard shadow-md rounded-lg border border-gray-200 flex flex-col gap-6">
 
             {/* Date Picker */}
-            <DatePicker />
+            <DatePickerCard />
+
 
             <div className="grid grid-cols-1 md:grid-cols-4 gap-5 rounded-md">
                 <ProductCard />
